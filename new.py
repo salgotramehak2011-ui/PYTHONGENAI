ec50337baba7dec25cd91f218a904e9c79ed44c7
salgotramehak2011-ui <salgotrakartik2011@gmail.com>
2026-01-28 11:56:36 +0530

    update now

--- new.py
+++ new.py
@@ -1,6 +1,9 @@
 import random
 print("Game Begin")
-user = input("Show the gesture of hand user: ")
+user = input("Show the gesture of user hand: ")
 print(user)
 comp = ["Rock","Paper","Scissor"]
 print(random.choice(comp))
+print("Win")
+print("Losser")
+print("MatchDraw")
