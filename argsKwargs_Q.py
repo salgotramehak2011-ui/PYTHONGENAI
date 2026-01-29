0dd025180c009298ceb46aca17143e8d63e64afe
salgotramehak2011-ui <salgotrakartik2011@gmail.com>
2026-01-29 18:41:54 +0530

    update now

--- argsKwargs_Q.py
+++ argsKwargs_Q.py
@@ -1,15 +1,28 @@
 # ----------*ARGS-------------
-def avg_marks(*a):
-    print(a)
-
-
+# def avg_marks(*a):
+#     total = 0
+#     count = 0
+#     for i in a:
+#         if i >= 0:
+#             total += i
+#             count += 1
+#     if count == 0:
+#         return 0
+#     return total / count
+# print(avg_marks(20,40,-1,55))
+  
 
 #--------**KWARGS-----------
-def filter_details(**a):
-    print(a)
-    
-      
-print(filter_details(name = "Mehak",course = "CSE",age = 20))    
+# def filter_details(**a):
+#     for key,value in a.items():
+#         if type(value) == str:
+#             print(key,"=",value)
+# filter_details(name = "Mehak", course = "CSE", age =20)
+
+
+
+
+   
 
 
 
