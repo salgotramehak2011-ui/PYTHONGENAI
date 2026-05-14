f19094d6a3123afc1b18154c0215c810819da819
salgotramehak2011-ui <salgotrakartik2011@gmail.com>
2026-05-14 11:25:51 +0530

    up langhugging

--- DSJUP/Lang/langhugging.py
+++ DSJUP/Lang/langhugging.py
@@ -5,12 +5,11 @@ load_dotenv()
 # import os
 st.header("Hugging Face ChatBOT")
 
-token = "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"
+
 
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.3-70B-Instruct:sambanova",
     task="text-generation",
-    huggingfacehub_api_token=token
 )
 model = ChatHuggingFace(llm=llm)
 input_text= st.text_input("Enter Your Question here")
