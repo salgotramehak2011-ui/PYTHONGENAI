eed5ee87f83933bca9e3822692f9fa6c9a594303
salgotramehak2011-ui <salgotrakartik2011@gmail.com>
2026-05-14 12:20:23 +0530

    new

--- DSJUP/Lang/langhugging.py
+++ DSJUP/Lang/langhugging.py
@@ -2,11 +2,11 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import streamlit as st 
 load_dotenv()
-# import os
-st.header("Hugging Face ChatBOT")   
+st.header("Hugging Face ChatBOT")
+
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.3-70B-Instruct:sambanova",
-    task="text-generation",
+    task="text-generation"
 )
 model = ChatHuggingFace(llm=llm)
 input_text= st.text_input("Enter Your Question here")
